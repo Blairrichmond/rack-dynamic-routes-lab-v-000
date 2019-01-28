7ca5ae6c9b7d54b4d2a8f0b52ecd23d5da7f1651
Brent Ransom <sadumedusa@gmail.com>
2019-01-28 00:44:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,13 +4,5 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
  
-    if req.path=="/items"
-      resp.write "You requested the songs"
-    else
-      resp.write "Route not found"
-      resp.status = 404
-    end
- 
-    resp.finish
-  end
+   
 end
